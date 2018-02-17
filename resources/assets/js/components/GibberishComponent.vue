3da49691d3db2c04ce5d329e737767195f6cfc19
piotrjankiewicz <piotr.jankiewicz@movecloser.pl>
2018-02-17 18:57:01 +0100

    for now I do vue in app.js, I dunno how to manage assets one js per page with webpack. I use app.js for all

---
+++ resources/assets/js/components/GibberishComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        It is a gibberish. But it's in vue!
+        <span class="badge badge-primary badge-pill">Vue</span>
+    </li>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.');
+        }
+    }
+</script>
--- resources/views/gibberish/index.blade.php
+++ resources/views/gibberish/index.blade.php
@@ -8,13 +8,9 @@
                     <div class="card-header">Heading</div>
                     <div class="card-body">
                         <ul class="list-group">
-                            <li class="list-group-item d-flex justify-content-between align-items-center">
-                                It is a gibberish. But it's in vue!
-                                <span class="badge badge-primary badge-pill">Vue</span>
-                            </li>
+                            <gibberish-component></gibberish-component>
                         </ul>
                     </div>
-                    <gibberish></gibberish>
                 </div>
             </div>
         </div>
