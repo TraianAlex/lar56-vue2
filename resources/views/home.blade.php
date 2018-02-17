b38fa04d5e22873dd8245fe7d577e1e65e06e419
piotrjankiewicz <piotr.jankiewicz@movecloser.pl>
2018-02-17 18:21:07 +0100

    gibberish project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+
+
+                    <div class="card-header">Dashboard</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <h5 class="card-title">You are logged in!</h5>
+                        <p class="card-text">Vue gibberish is our passion.</p>
+                        <a href="#" class="btn btn-primary">Go gibber somewhere!</a>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
