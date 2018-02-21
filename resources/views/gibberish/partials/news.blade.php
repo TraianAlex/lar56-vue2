9e48fdcb11dde42a4c9be858b75cf46abbb06fb6
piotrjankiewicz <piotr.jankiewicz@movecloser.pl>
2018-02-21 20:02:23 +0100

    put everything into partials and add few more messages to news tab

--- resources/views/gibberish/partials/news.blade.php
+++ resources/views/gibberish/partials/news.blade.php
@@ -3,6 +3,11 @@
         <div class="col-md-12">
             <message title="The Guacamole news"
                      body="'Yupi ca yey. Lorem ipsum dolor sit amet. Gibber spreads around the world!' - Today,  by me."></message>
+            <message title="NewsYorkers"
+                     body="'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+                     eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' - Today,  by my granma."></message>
+            <message title="See-N-N"
+                     body="'Yupi lababdad ypicayey ehhooo hohoh hihi huhuh lool troloolol xd wtf amazing srsly xd' - Today,  by me."></message>
         </div>
     </div>
 </div>
