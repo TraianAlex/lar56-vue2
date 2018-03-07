29bc58ac7e7e0f2f10dd6ad1f50c2b9009a881eb
sweettirexkiller <piotrjankiewicz02@gmail.com>
2018-03-07 20:34:26 +0100

    named slots in a nutshell

--- resources/views/gibberish/partials/scoped.blade.php
+++ resources/views/gibberish/partials/scoped.blade.php
@@ -2,8 +2,8 @@
     <div class="row">
         <div class="col-md-12">
             <scoped-slot :items="['one','two','three']">
-                <template scope="{bar}">
-                    <h4 v-text="props.bar"></h4>
+                <template scope="props">
+                    <h4 v-text="props.bar"></h4><p v-text="props.item"></p>
                 </template>
             </scoped-slot>
         </div>
